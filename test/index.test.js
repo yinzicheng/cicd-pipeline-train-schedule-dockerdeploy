d5e0488b36b8fd944a902c397808655e9ee6346e
zicheng yin <yinzicheng@gmail.com>
2020-10-02 08:51:37 +1000

    update Jenkinsfile

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,22 @@
 var request = require('supertest');
 var express = require('express');
- 
+
 var app = require('../app');
- 
-describe('Index Page', function() {
-  it("renders successfully", function(done) {
-    request(app).get('/').expect(200, done);    
-  })
+
+describe('Index Page', function () {
+    it("renders successfully", function (done) {
+        request(app).get('/').expect(200, done);
+    })
 })
 
-describe('Trains API', function() {
-  it("returns data successfully", function(done) {
-    request(app).get('/trains').expect(200).expect('Content-Length', '1093').expect('Content-Type', /json/, done); ;    
-  })
+describe('Trains API', function () {
+    it("returns data successfully", function (done) {
+        request(app).get('/trains').expect(200).expect('Content-Length', '1093').expect('Content-Type', /json/, done);
+    })
+})
+
+describe('Failed Test', function () {
+    it("returns data successfully", function (done) {
+        request(app).get('/abc').expect(200).expect('Content-Length', '1093').expect('Content-Type', /json/, done);
+    })
 })
